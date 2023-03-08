d13ff98a3f6abe76c175b26d1dd4d5d332ab22fd
Kamron770 <123852911+Kamron770@users.noreply.github.com>
2023-03-08 16:35:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var num1 = +prompt('ВВедите первое число');
 var num2 = +prompt('ВВедите второе число');
 var num3 = +prompt('ВВедите третье число');
 if(num1 > num2 && num1 < num3){
-    alert(num1 + ' Наибольшее число');
+    alert(num1 + '  Среднее число');
 }
 else if(num2 > num1 && num2 < num3){
     alert(num2 + ' Среднее число');
 }
 else if(num3 > num1 && num3 < num2){
-    alert(num3 + ' Наибольшее число');
+    alert(num3 + '  Среднее число');
 }
 else{
     alert('Error');
